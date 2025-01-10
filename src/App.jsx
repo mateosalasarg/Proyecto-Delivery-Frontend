962e0b91ee2682632ef228d9e4fddcd1657723a2
mateosalasarg <texplorador135@gmail.com>
2025-01-10 03:30:01 -0300

    Iniciado el enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Nav from './components/Nav/Nav'
-
+import {Routes} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Contact from './pages/Contact/Contact'
 const App = () => {
   return (
     <div className='App'>
       <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
