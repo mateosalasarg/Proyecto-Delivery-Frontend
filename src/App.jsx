11f92d827f4499c2fb22aea22a3142d2b65fe454
matiasgasparg <matiasgasparg@gmail.com>
2025-01-15 19:51:06 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { AuthProvider, AuthContext } from './auth/AuthContext';
 import Nav from './components/Nav/Nav';
@@ -7,10 +7,10 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import Order from './pages/Order/Order';
 import Login from './pages/Login/Login';
-import LoginPage from './pages/admin/LoginPage';
+import LoginPage from './pages/admin/LoginPage'; // Importamos LoginPage
 import DashboardPage from './pages/Admin/DashboardPage';
-import Pedidos from './pages/Admin/Pedidos';
-import Platos from './pages/Admin/Platos';
+import Pedidos from './pages/Admin/Pedidos'
+import Platos from './pages/Admin/Platos'
 
 // Componente para proteger rutas
 const ProtectedRoute = ({ children }) => {
@@ -24,31 +24,26 @@ const ProtectedRoute = ({ children }) => {
 };
 
 ProtectedRoute.propTypes = {
-    children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired, // Valida que 'children' sea un nodo React
 };
 
 const App = () => {
-    const location = useLocation();
-
-    // Rutas donde no se muestra el Nav
-    const hideNavRoutes = ["/", "/admin/login"];
-
     return (
         <AuthProvider>
             <div className="App">
-                {/* Renderizar Nav solo si no estamos en rutas específicas */}
-                {!hideNavRoutes.includes(location.pathname) && <Nav />}
+                <Nav /> {/* Barra de navegación */}
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/admin/login" element={<LoginPage />} />
+                    {/* Ruta para el login de administrador */}
+                    <Route path="/admin/login" element={<LoginPage />} /> 
                     <Route
-                        path="/dashboard"
-                        element={
-                            <ProtectedRoute>
-                                <DashboardPage />
-                            </ProtectedRoute>
-                        }
-                    />
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <DashboardPage /> {/* Asegúrate de que este componente esté definido */}
+                        </ProtectedRoute>
+                    }
+                />
+                    {/* Rutas protegidas para el usuario autenticado */}
                     <Route
                         path="/home"
                         element={
@@ -77,7 +72,7 @@ const App = () => {
                         path="/admin/pedidos"
                         element={
                             <ProtectedRoute>
-                                <Pedidos />
+                                <Pedidos /> {/* Componente donde se muestran los pedidos */}
                             </ProtectedRoute>
                         }
                     />
@@ -85,10 +80,15 @@ const App = () => {
                         path="/admin/platos"
                         element={
                             <ProtectedRoute>
-                                <Platos />
+                                <Platos /> {/* Componente donde se muestran los platos */}
                             </ProtectedRoute>
                         }
                     />
+
+                    {/* Ruta para el login general */}
+                    <Route path="/" element={<Login />} />
+
+
                 </Routes>
             </div>
         </AuthProvider>
