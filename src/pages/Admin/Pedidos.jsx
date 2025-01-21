40f58bc8a748aedcdf32ae74c68af58ea2ccc46a
matiasgasparg <matiasgasparg@gmail.com>
2025-01-21 11:38:17 -0300

    updateado pedido

--- src/pages/Admin/Pedidos.jsx
+++ src/pages/Admin/Pedidos.jsx
@@ -11,7 +11,8 @@ const Pedidos = () => {
     const [availablePlatos, setAvailablePlatos] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedPlato, setSelectedPlato] = useState(null);
-
+    const [updateField, setUpdateField] = useState('');
+    const [updateValue, setUpdateValue] = useState('');
     useEffect(() => {
         const fetchPedidos = async () => {
             try {
@@ -135,97 +136,177 @@ const Pedidos = () => {
             alert('Error al eliminar el plato');
         }
     };
+    // Función para actualizar el pedido
+    const updatePedido = async (id_pedido) => {
+        if (!updateField || !updateValue) {
+            alert('Por favor seleccione un campo y proporcione un valor.');
+            return;
+        }
+
+        // Convertir 'pagado' a 1 o 2
+        let valorFinal = updateValue;
+        if (updateField === 'pagado') {
+            valorFinal = updateValue === 'Pagado' ? 1 : 2;  // Convertimos a 1 o 2
+        
+        }
+
+        try {
+            await axios.put(`http://127.0.0.1:5000/pedidos/${id_pedido}`, {
+                field: updateField,
+                value: valorFinal, // Enviamos el valor numérico
+            });
+
+            setPedidos((prevPedidos) =>
+                prevPedidos.map((pedido) =>
+                    pedido.id_pedido === id_pedido
+                        ? { ...pedido, [updateField]: valorFinal }
+                        : pedido
+                )
+            );
+
+            alert('Pedido actualizado con éxito.');
+            setUpdateField('');
+            setUpdateValue('');
+        } catch (err) {
+            console.error('Error al actualizar el pedido:', err);
+            alert('Error al actualizar el pedido.');
+        }
+    };
 
 
     if (loading) return <div>Cargando pedidos...</div>;
     if (error) return <div>{error}</div>;
-
-    return (
-        <div>
-            <h1>Pedidos</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Cliente</th>
-                        <th>Fecha</th>
-                        <th>Estado</th>
-                        <th>Comentario</th>
-                        <th>Domicilio</th>
-                        <th>Detalles</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pedidos.map((pedido) => (
-                        <React.Fragment key={pedido.id_pedido}>
+// Función para actualizar el pedido
+return (
+    <div>
+        <h1>Pedidos</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Cliente</th>
+                    <th>Fecha</th>
+                    <th>Estado</th>
+                    <th>Comentario</th>
+                    <th>Domicilio</th>
+                    <th>Detalles</th>
+                </tr>
+            </thead>
+            <tbody>
+                {pedidos.map((pedido) => (
+                    <React.Fragment key={pedido.id_pedido}>
+                        <tr>
+                            <td>{pedido.id_cliente}</td>
+                            <td>{new Date(pedido.fecha_hora).toLocaleString()}</td>
+                            <td>{pedido.estado}</td>
+                            <td>{pedido.comentario}</td>
+                            <td>{pedido.domicilio_entrega}</td>
+                            <td>
+                                <button onClick={() => toggleDetalles(pedido.id_pedido)}>
+                                    {expandedPedidoId === pedido.id_pedido ? 'Ocultar detalles' : 'Ver detalles'}
+                                </button>
+                                <button onClick={() => setIsModalOpen(true)}>Agregar Plato</button>
+                            </td>
+                        </tr>
+                        {expandedPedidoId === pedido.id_pedido && (
                             <tr>
-                                <td>{pedido.id_cliente}</td>
-                                <td>{new Date(pedido.fecha_hora).toLocaleString()}</td>
-                                <td>{pedido.estado}</td>
-                                <td>{pedido.comentario}</td>
-                                <td>{pedido.domicilio_entrega}</td>
-                                <td>
-                                    <button onClick={() => toggleDetalles(pedido.id_pedido)}>
-                                        {expandedPedidoId === pedido.id_pedido ? 'Ocultar detalles' : 'Ver detalles'}
-                                    </button>
-                                    <button onClick={() => setIsModalOpen(true)}>Agregar Plato</button>
+                                <td colSpan="6">
+                                    <h3>Detalles del Pedido:</h3>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Cantidad</th>
+                                                <th>Comentario</th>
+                                                <th>Nombre del Plato</th>
+                                                <th>Precio</th>
+                                                <th>Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {pedido.detalles.map((detalle) => (
+                                                <tr key={detalle.id_pedido_detalle}>
+                                                    <td>{detalle.cantidad}</td>
+                                                    <td>{detalle.comentario}</td>
+                                                    <td>{platosDetalles[detalle.id_plato]?.nombre || 'Cargando...'}</td>
+                                                    <td>{platosDetalles[detalle.id_plato]?.precio || 'Cargando...'}</td>
+                                                    <td>
+                                                        <button onClick={() => removePlatoFromPedido(pedido.id_pedido, detalle.id_plato)}>Eliminar Plato</button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+
+                                    <h3>Actualizar Pedido</h3>
+                                    <form
+                                        onSubmit={(e) => {
+                                            e.preventDefault();
+                                            updatePedido(pedido.id_pedido);
+                                        }}
+                                    >
+                                        <label htmlFor={`field-${pedido.id_pedido}`}>Campo:</label>
+                                        <select
+                                            id={`field-${pedido.id_pedido}`}
+                                            value={updateField}
+                                            onChange={(e) => setUpdateField(e.target.value)}
+                                        >
+                                            <option value="">Seleccionar campo</option>
+                                            <option value="estado">Estado</option>
+                                            <option value="comentario">Comentario</option>
+                                            <option value="id_repartidor">ID Repartidor</option>
+                                            <option value="pagado">Pagado</option>
+                                        </select>
+
+                                        <label htmlFor={`value-${pedido.id_pedido}`}>Nuevo Valor:</label>
+                                        {updateField === 'pagado' ? (
+                                            <select
+                                                id={`value-${pedido.id_pedido}`}
+                                                value={updateValue}
+                                                onChange={(e) => setUpdateValue(e.target.value)}
+                                            >
+                                                <option >...</option>
+
+                                                <option value="Pagado">Pagado</option>
+                                                <option value="No pagado">No pagado</option>
+                                            </select>
+                                        ) : (
+                                            <input
+                                                id={`value-${pedido.id_pedido}`}
+                                                type="text"
+                                                value={updateValue}
+                                                onChange={(e) => setUpdateValue(e.target.value)}
+                                            />
+                                        )}
+
+                                        <button type="submit">Actualizar</button>
+                                    </form>
                                 </td>
                             </tr>
-                            {expandedPedidoId === pedido.id_pedido && (
-                                <tr>
-                                    <td colSpan="6">
-                                        <h3>Detalles del Pedido:</h3>
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th>Cantidad</th>
-                                                    <th>Comentario</th>
-                                                    <th>Nombre del Plato</th>
-                                                    <th>Precio</th>
-                                                    <th>Acciones</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {pedido.detalles.map((detalle) => (
-                                                    <tr key={detalle.id_pedido_detalle}>
-                                                        <td>{detalle.cantidad}</td>
-                                                        <td>{detalle.comentario}</td>
-                                                        <td>{platosDetalles[detalle.id_plato]?.nombre || 'Cargando...'}</td>
-                                                        <td>{platosDetalles[detalle.id_plato]?.precio || 'Cargando...'}</td>
-                                                        <td>
-                                                            <button onClick={() => removePlatoFromPedido(pedido.id_pedido, detalle.id_plato)}>Eliminar Plato</button>
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </tbody>
-            </table>
+                        )}
+                    </React.Fragment>
+                ))}
+            </tbody>
+        </table>
 
-            {isModalOpen && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h3>Seleccionar Plato para agregar</h3>
-                        <select onChange={(e) => setSelectedPlato(JSON.parse(e.target.value))} value={selectedPlato ? JSON.stringify(selectedPlato) : ''}>
-                            <option value="">Seleccionar Plato</option>
-                            {availablePlatos.map(plato => (
-                                <option key={plato.id_plato} value={JSON.stringify(plato)}>
-                                    {plato.nombre} - ${plato.precio} - {plato.disponible === 1 ? 'Disponible' : 'No disponible'}
-                                </option>
-                            ))}
-                        </select>
-                        <br />
-                        <button onClick={() => addPlatoToPedido(expandedPedidoId)}>Agregar Plato</button>
-                        <button onClick={() => setIsModalOpen(false)}>Cerrar</button>
-                    </div>
+        {isModalOpen && (
+            <div className="modal">
+                <div className="modal-content">
+                    <h3>Seleccionar Plato para agregar</h3>
+                    <select onChange={(e) => setSelectedPlato(JSON.parse(e.target.value))} value={selectedPlato ? JSON.stringify(selectedPlato) : ''}>
+                        <option value="">Seleccionar Plato</option>
+                        {availablePlatos.map(plato => (
+                            <option key={plato.id_plato} value={JSON.stringify(plato)}>
+                                {plato.nombre} - ${plato.precio} - {plato.disponible === 1 ? 'Disponible' : 'No disponible'}
+                            </option>
+                        ))}
+                    </select>
+                    <br />
+                    <button onClick={() => addPlatoToPedido(expandedPedidoId)}>Agregar Plato</button>
+                    <button onClick={() => setIsModalOpen(false)}>Cerrar</button>
                 </div>
-            )}
-        </div>
-    );
+            </div>
+        )}
+    </div>
+);
 };
 
-export default Pedidos;
+export default Pedidos; 
